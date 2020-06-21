9deaf947fc30deb5df15f35b477b2fe6d22da579
riteshtikare <60392084+riteshtikare@users.noreply.github.com>
2020-06-21 13:46:15 +0530

    first

---
+++ user tab/script.js
@@ -0,0 +1,4 @@
+gsap.from('#nav a,button',{ stagger:.3,duration: 1, ease:'expo.inOut' , x: 40, opacity: 0});
+gsap.from('.links',{ stagger:.3,duration: 1, ease:'expo.inOut' , x: 40, opacity: 0});
+
+gsap.from('#right',{ duration: .5, ease:'expo.inOut' , x: 40, opacity: 0});
